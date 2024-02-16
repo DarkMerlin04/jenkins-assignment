81b79f39c25acd4ab6bbbfff20f8d6d964a29806
Dark Merlin <ajdvdsf.aj@gmail.com>
2024-02-17 00:41:16 +0700

    update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req,res) => {
 })
 
 app.get('/getcode', (req,res)=> {
-    res.send("64010718 Raweeroj Thokaeo (AJ)")
+    res.send("Congratulations!!!")
 })
 
 app.get('/plus/:num1/:num2', (req,res) => {
